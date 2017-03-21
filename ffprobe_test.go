ecde7507446817d26297df7a47f6a5ce46848b77
Matt Joiner <anacrolix@gmail.com>
2017-03-21 13:27:36 +1100

    Add TestEmptyFile and move Info test to info_test.go

--- ffprobe_test.go
+++ ffprobe_test.go
@@ -1,30 +1,19 @@
 package ffprobe
 
 import (
+	"fmt"
+	"io/ioutil"
+	"os"
 	"testing"
-	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-func TestInfoHelperMethods(t *testing.T) {
-	for _, tc := range []struct {
-		Info          // Input data
-		time.Duration // Expected duration
-		uint          // Expected bitrate
-	}{
-		{Info{
-			Format: map[string]interface{}{"duration": "N/A"},
-		}, 0, 0},
-		{Info{Format: map[string]interface{}{"bit_rate": "128000.000000", "duration": "N/A"}}, 0, 128000},
-		{Info{}, 0, 0},
-		{Info{Format: map[string]interface{}{"duration": "1377.628452"}}, 1377628452000, 0},
-	} {
-		br, _ := tc.Bitrate()
-		if br != tc.uint {
-			t.Fatalf("bitrate is %d, but expected %d", br, tc.uint)
-		}
-		d, _ := tc.Info.Duration()
-		if d != tc.Duration {
-			t.Fatalf("duration is %s but expected %s", d, tc.Duration)
-		}
-	}
+func TestEmptyFile(t *testing.T) {
+	f, err := ioutil.TempFile("", "")
+	require.NoError(t, err)
+	defer os.Remove(f.Name())
+	_, err = Run(f.Name())
+	assert.EqualError(t, err, fmt.Sprintf("exit status 1: %s: Invalid data found when processing input", f.Name()))
 }
